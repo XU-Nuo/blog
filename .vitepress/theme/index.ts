3a42cc3e302a0c23c85b71da12a5bc10d6c03cc8
XUNuo <xunuo@sribs.com>
2024-04-18 02:09:33 +0000

    continue layout fix

--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -1,16 +1,15 @@
 import { h } from 'vue'
 import Theme from 'vitepress/theme'
 import './style/vars.css'
-import LayoutT from './Layout.vue'
+import MLayout from './components/MLayout.vue'
 
 import RegisterSW from './components/RegisterSW.vue'
 
 export default {
   ...Theme,
   Layout() {
-    return h(Theme.Layout, null, {
+    return h(MLayout, null, {
       'layout-bottom': () => h(RegisterSW)
     })
   },
-  LayoutT
 }
