2d2577b696f299a6658cc3246c16b6765f02d108
XUNuo <xunuo@sribs.com>
2024-04-18 02:04:53 +0000

    fix transition import

--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -1,7 +1,7 @@
 import { h } from 'vue'
 import Theme from 'vitepress/theme'
 import './style/vars.css'
-import Layout from './Layout.vue'
+import LayoutT from './Layout.vue'
 
 import RegisterSW from './components/RegisterSW.vue'
 
@@ -11,5 +11,6 @@ export default {
     return h(Theme.Layout, null, {
       'layout-bottom': () => h(RegisterSW)
     })
-  }
+  },
+  LayoutT
 }
